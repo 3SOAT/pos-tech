8a55694ae2add7b6a23307066ffd3ea2f6d2318b
aline.tanure <aline.tanure@ifood.com.br>
2023-10-10 10:17:44 -0300

    Start Spring Boot Application

---
+++ src/main/java/br/com/fiap/pos/soat3/lanchonete/LanchoneteApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pos.soat3.lanchonete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchoneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchoneteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/pos/soat3/lanchonete/LanchoneteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pos.soat3.lanchonete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchoneteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
